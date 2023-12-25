39eb318691a1bd56172c30cedf4ce23b0d06e0cf
monzurul002 <monzurul002@gmail.com>
2023-12-25 21:58:37 +0600

    await client remove

--- index.js
+++ index.js
@@ -52,7 +52,7 @@ const client = new MongoClient(uri, {
 async function run() {
     try {
         // Connect the client to the server	(optional starting in v4.7)
-        await client.connect();
+        // await client.connect();
 
         const cartCollection = client.db("muscleDb").collection("cart");
         const classesCollection = client.db("muscleDb").collection("classes");
